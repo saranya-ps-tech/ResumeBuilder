5cb66d8a4bbcb22387f4923f56b6ff8ca4a3df58
Saranya P S <saranyasasikumar619@gmail.com>
2025-08-11 10:59:32 +0530

    resume building day 1

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+
+import './App.css'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import History from './pages/History';
+import PageNotFount from './pages/PageNotFount';
+import ResumeGenerator from './pages/ResumeGenerator';
+import UserForm from './pages/UserForm';
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/resume' element={<ResumeGenerator />} />
+        <Route path='/form' element={<UserForm />} />
+        <Route path='/history' element={<History />} />
+        <Route path='/*' element={<PageNotFount />} />
+
+      </Routes>
+
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/PageNotFount.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PageNotFount() {
+  return (
+    <div>PageNotFount</div>
+  )
+}
+
+export default PageNotFount
